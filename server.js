4ceb49f7434940c3ab8f5c52176145a7f241800f
Bivvon Kinanga <151150127+bivvon@users.noreply.github.com>
2026-06-10 20:05:54 +0300

    Enhance CORS settings in server.js
    Updated CORS configuration to specify allowed origins, methods, and headers.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 const EP     = 'https://api.elementpay.net/api/v1';
